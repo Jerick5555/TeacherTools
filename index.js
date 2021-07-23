7d77f57af07913a6008e4b284d5b13cff74597c6
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2021-07-23 19:50:52 +0800

    start

---
+++ index.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const mongoose = require('mongoose');
+const client = new Discord.Client();
+const { prefix } = require('./config.json');
+const fs = require('fs');
+client.mongoose = require('./utils/mongoose');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    exports.client = client;
+});
+
+// Tells us how many servers have our bot
+setInterval(botStatus, 60000);
+function botStatus() {
+    client.user.setActivity(client.guilds.cache.size + " servers| -help for help");
+}
+
+client.login(process.env.token);
+
+client.on('message', message => {
+    // Check if the message was sent by the bot
+    if (message.author.bot) return;
+
+    // Check if message starts with the prefix
+    if (!message.content.startsWith(prefix)) return;
+    const args = message.content.slice(prefix.length).match(/\S+/g);
+    const commandName = args.shift().toLowerCase();
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    if (!command) {
+        message.channel.send(`Invalid command. Type ${prefix}help for commands to use.`);
+    }
+    else {
+        command.execute(message, args);
+    }
+});
---
+++ utils/mongoose.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+module.exports = {
+    init: () => {
+        const dbOptions = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            autoIndex: false, // Don't build indexes
+            poolSize: 10, // Maintain up to 10 socket connections
+            serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+            family: 4 // Use IPv4, skip trying IPv6
+        };
+         // Somehow this works and connects so don't change it thanks
+        mongoose.connect("mongodb+srv://Jerick:JerickPassword@teachertools.eojsd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", dbOptions);
+        mongoose.set('useFindAndModify', false);
+        mongoose.Promise = global.Promise;
+
+        mongoose.connection.on('connected', () => {
+            console.log('Mongoose has successfully connected!');
+        });
+
+        mongoose.connection.on('err', err => {
+            console.error(`Mongoose connection error: \n${err.stack}`);
+        });
+
+        mongoose.connection.on('disconnected', () => {
+            console.warn('Mongoose connection lost');
+        });
+    }
+}
