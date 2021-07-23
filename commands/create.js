9fb23a17cad017bf9f3467a0add6d1332e03e35b
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2021-07-23 22:12:07 +0800

    expanded charset

--- commands/create.js
+++ commands/create.js
@@ -15,7 +15,7 @@ module.exports = {
                 async function generatePassword() {
                     while (true) {
                         var length = 8,
-                        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+                        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-+=_",
                         retVal = "";
                         for (var i = 0, n = charset.length; i < length; ++i) {
                             retVal += charset.charAt(Math.floor(Math.random() * n));
