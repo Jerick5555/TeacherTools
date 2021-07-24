eb2bbc9da64295063cc072d15c1edef24b83d611
Jerick5555 <69852317+Jerick5555@users.noreply.github.com>
2021-07-24 10:12:22 +0800

    auto creates user

--- commands/join.js
+++ commands/join.js
@@ -3,6 +3,7 @@ const Class = require('../models/class');
 const mongoose = require('mongoose');
 const Discord = require('discord.js');
 const room = require('../models/room');
+const User = require('../models/user');
 
 module.exports = {
     name: "join",
@@ -53,8 +54,25 @@ module.exports = {
                                     .then(result => console.log(result))
                                     .catch(err => console.error(err));
                                 message.channel.send(`You have successfully joined the room!`);
-                                User.findOne({ id: message.author.id }, (err, user) => {
 
+                                // Changes current class code to the one joined
+                                User.findOne({ id: message.author.id }, (err, user) => {
+                                    if (err) console.log(err);
+                                    if (user != null) {
+                                        user.currentClassCode = args[1];
+                                    }
+                                    else{
+                                        // Makes user if it does not exist
+                                        user = new User({
+                                            _id: mongoose.Types.ObjectId(),
+                                            userID: message.author.id,
+                                            currentClassCode: args[1]
+                                        });
+                
+                                        user.save()
+                                            .then(result => console.log(result))
+                                            .catch(err => console.error(err));
+                                    }
                                 });
                             }
                             else {
